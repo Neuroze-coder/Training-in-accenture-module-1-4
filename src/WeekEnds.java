9d399bee1784ddd1e318a169d6120eb7404fa1b7
Neuroze <Speransky.java@gmail.com>
2021-10-05 20:19:26 +0300

    Added a solution on the task "WeekEnds"

--- src/WeekEnds.java
+++ src/WeekEnds.java
@@ -1,3 +1,7 @@
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.*;
+
 /**
  * Необходимо реализовать метод , определяющий относиться ли параметр dataTime к выходным
  * Выходным считается промежуток между 22:00 пятницы и 23:00 воскресенья
@@ -5,7 +9,41 @@
  */
 
 public class WeekEnds {
+
+    private static int friday = 5;
+    private static int saturday = 6;
+    private static int sunday = 7;
+    private static int weekendStart = 22;
+    private static int weekendEnd = 23;
+
     public static void main(String[] args) {
-        //TODO
+
+        List <LocalDateTime> dateTimeList = new ArrayList<>();
+        dateTimeList.add(LocalDateTime.of(2021,10,1,21,50));
+        dateTimeList.add(LocalDateTime.of(2021,10,1,22,1));
+        dateTimeList.add(LocalDateTime.of(2021,10,2,14,10));
+        dateTimeList.add(LocalDateTime.of(2021,10,3,22,59));
+        dateTimeList.add(LocalDateTime.of(2021,10,3,23,12));
+        dateTimeList.add(LocalDateTime.of(2021,10,4,10,15));
+        dateTimeList.add(LocalDateTime.of(2021,10,5,12,15));
+        dateTimeList.add(LocalDateTime.of(2021,10,6,12,15));
+        dateTimeList.add(LocalDateTime.of(2021,10,9,12,15));
+
+        for (LocalDateTime dateTime : dateTimeList) {
+            System.out.println(dateTime + " выходной? " + isWeekendDay(dateTime));
+        }
+
+    }
+
+    public static boolean isWeekendDay(LocalDateTime dateTime) {
+        if (dateTime.getDayOfWeek().getValue() == saturday) {
+            return true;
+        } else if ((dateTime.getDayOfWeek().getValue() == friday) & (dateTime.getHour() >= weekendStart)) {
+            return true;
+        } else if (dateTime.getDayOfWeek().getValue() == sunday & dateTime.getHour() < weekendEnd) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
