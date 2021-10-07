3435f5c6ad2b2b07ae2a77a376cd9ccd8e20eba7
ilya.speransky <Speransky.java@gmail.com>
2021-10-08 00:51:15 +0300

    Added a solution on the task "MyComparator"

---
+++ src/MaxValue.java
@@ -0,0 +1,13 @@
+public class MyComparator {
+    /**
+     * Реализовать метод, возвращающий максимальное число
+     */
+    public static void main(String[] args) {
+        System.out.println(Math.max(10, 12));
+    }
+
+    public int max(int a, int b) {
+        Math.max(a, b);
+        return 0;
+    }
+}
