bc5f1ba96eedf03cc62c446889df59ee51a1fbba
ilya.speransky <Speransky.java@gmail.com>
2021-10-08 00:53:57 +0300

    Added a solution on the task "MyComparator"

--- src/MyComparator.java
+++ src/MyComparator.java
@@ -3,7 +3,7 @@ public class MyComparator {
      * Реализовать метод, возвращающий максимальное число
      */
     public static void main(String[] args) {
-        System.out.println(Math.max(10, 12));
+        System.out.println(Math.max(10, 15));
     }
 
     public int max(int a, int b) {
